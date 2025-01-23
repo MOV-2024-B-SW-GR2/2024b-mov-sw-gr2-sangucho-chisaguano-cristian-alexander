222f0c23d847267903083a76edeac58f2d24f873
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-01-23 14:30:12 -0500

    build: initial build

---
+++ sistemasolarcrud/app/src/main/java/com/example/sistema_solar_crud/PlanetaActivity.kt
@@ -0,0 +1,42 @@
+package com.example.sistema_solar_crud
+
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PlanetaActivity : AppCompatActivity() {
+    private lateinit var btnAgregarPlaneta: ListView
+    private lateinit var listViewPlanetas: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_planeta)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        listViewPlanetas = findViewById(R.id.ls_planetas)
+        btnAgregarPlaneta = findViewById(R.id.btn_crear_p)
+
+        registerForContextMenu(listViewPlanetas)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.menu_p, menu)
+
+    }
+}
